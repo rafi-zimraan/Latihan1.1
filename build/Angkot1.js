6836cf292dbcae54dce298ec47c5c0c364354b8b
rafi-zimraan <raffizimraan27@gmail.com>
2023-02-08 17:23:16 +0700

    menambah

--- build/Angkot1.js
+++ build/Angkot1.js
@@ -1,5 +1,5 @@
 "use strict";
-const jmlAngkot = 200;
+const jmlAngkot = 10;
 let noAngkot = 1;
 while (noAngkot <= jmlAngkot) {
     console.log('Angkot No.' + noAngkot + ' beroperasi dengan baik');
--- build/Angkot2.js
+++ build/Angkot2.js
@@ -1,1 +1,11 @@
 "use strict";
+let jumlahangkot = 10;
+let angkotBeroperasi = 6;
+let nomerAngkot = 1;
+while (nomerAngkot <= angkotBeroperasi) {
+    console.log('Angkot No. ' + nomerAngkot + ' beroperasi dengan baik');
+    nomerAngkot++;
+}
+for (let i = 7; i <= 10; i++) {
+    console.log(` Angkot No. ${i} Tidak beroperasi dengan baik`);
+}
